06278d6bdb78b55908f40f457b2e16404d9641cc
keblak <168828596+keblak@users.noreply.github.com>
2024-07-26 13:49:06 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,9 +20,9 @@ function convertToBool(text, fault = 'true') {
 
 global.thumb = fs.readFileSync('./lib/bugs/XByte.png') //Don't change
 //////////////////////////////////////////////////////////
-const BOTNAME = 'X - B Y T E'; //BOT name
+const BOTNAME = 'X - HYDRA; //BOT name
 //////////////////////////////////////////////////////////
-const FOOTERNAME = '*Powered by TalkDrove*'; //Footer text
+const FOOTERNAME = '*HYDRA*'; //Footer text
 
 
 
@@ -37,7 +37,7 @@ const FOOTERNAME = '*Powered by TalkDrove*'; //Footer text
 //----------------OWNER NUMBER------------------------------//
 //////////////////////////////////////////////////////////////////
 
-global.owner = process.env.OWNER_NUMBER  || '923072380380' ;  //put your number without +
+global.owner = process.env.OWNER_NUMBER  || '254783416040' ;  //put your number without +
 
 /////////////////////////////////////////////////////////////////
 //-------------------------------------------------------//
@@ -59,7 +59,7 @@ module.exports = { //exporting modules to use in plugins
 //----------------Session ID------------------------------//
 //////////////////////////////////////////////////////////////////
 
-SESSION_ID: process.env.SESSION_ID || 'Paste session id', //Paste your session ID here
+SESSION_ID: process.env.SESSION_ID || 'Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoieUFWQ1JrM3RPcnFyRHBBL1I1WHEzenN2WFJoRnFMYlJOdXVlNWpadnYyTT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTUQ0ZGVtQ3NidGlkSytuYkhRdC94TE91K0UzSUVpcFFNa2c2djN1dFRBWT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJjR3Z2WDY2YlB4Z0Z5Q2xEY3pTUUg0d1lzZ2lnOW9GZ0d3enVxRUQ5Y2xRPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJDMkdEWmJzQTZ6d0lnS0RTWmNsaGJWRy9xeUlQWFlaY1NpWUxYVCtUemdBPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ik9CZnFkMWd0dFhrSEQ0ZjhOWVAreWZ3N1lYRGZhQkN4UTJiMTQyKzZ4VlE9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImM5R1JnWXJlQVFXSHJRazZ5ZVRjV0pHT2NmQUJHTUFMVm00Wk9rNTErMlk9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUUdQUGtEYngwbDlIZlk1bFk0ZE45ZUZwNW9OZ2VZUk9OKzEvVVArZWZuUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiVXVWNE1IaGRYeWE1ZDhXZmdZUEhPY3hmVXZYOUlFdHNDd1IyMU44SXdDOD0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImRDS1cyNTlzZmhmbUFNRDB0QUhCV1JkWlNxOVJacGh1ZDFwVTEwdlU5aWt1a1JpeFZ2OEduTnNlMWhtcFJraU1vSERzTXNMbGloMzFSNFp1ZVY5d2hRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6NzksImFkdlNlY3JldEtleSI6Ik5waktUSXBnVlR2aFFZa3ZPaGx3WUhFK1NYVTcvMmcwYzh6UjQxZUxWbE09IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6IjFMVWlRUW1WUzVPVEhzME9JdmVLc3ciLCJwaG9uZUlkIjoiYzUxMDdkYWEtM2FiZi00MDA2LWI1NTctYTAyYjk0MjA4N2EzIiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlBNRlRQVmRsSXNIM3BtVDJXL1JZNWphN2ZqQT0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJmWHRXRnhSNmR4ek13VEpneDJiRGhveW5lUlE9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiNTVGRVRXSzIiLCJtZSI6eyJpZCI6IjI1NDc4MzQxNjA0MDo4QHMud2hhdHNhcHAubmV0IiwibmFtZSI6IuC8kuC8kuKEguKEkuGOveKEleKEsOC8kuC8kiJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDTzZzaFo4Q0VLYjdqYlVHR0FFZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiNUFoY21OcjN0eXB1cDJxbmVxVXJSSXUxQnd2L0pmWFdmMEszaWUvQ0ExST0iLCJhY2NvdW50U2lnbmF0dXJlIjoiZjNkRnE2dTFTNnZKZWVvM0xlZU85ZEp5ZE5pVUJES3RFVUJQNjVhVHZZYzc2eUVHTzNLVFZFYkI3WGZVMnZXVVpBNGZjQlVsa2NSK0tLZVlwVWFKQ2c9PSIsImRldmljZVNpZ25hdHVyZSI6Im1WUzRFMVBzT1dDajh0K0JlSCtQRDhuRHU5SzU1WCtJa0xUcXI3QmM4SWNZNE9QdXd5dW1ZZmd6d2I5YUpEUVZDbGVRQ2J6Sk5CMnpaRHRTdlowOGpBPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjU0NzgzNDE2MDQwOjhAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCZVFJWEpqYTk3Y3FicWRxcDNxbEswU0x0UWNML3lYMTFuOUN0NG52d2dOUyJ9fV0sInBsYXRmb3JtIjoic21iYSIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcyMTk5MDU3OX0=', //Paste your session ID here
 
 /////////////////////////////////////////////////////////////////
 //-------------------------------------------------------//
@@ -86,7 +86,7 @@ LOGO: process.env.LOGO || `https://raw.githubusercontent.com/HyHamza/HyHamza/mai
 LANG:  process.env.LANG || 'EN' , //URDU OR EN
 //////////////////////////////////////////////////////////
 
-PREFIX: process.env.PREFIX || '.' , //PREFIX, LEAVE IT DEFAULT OTHERWISE BUTTON WILL NOT WORK
+PREFIX: process.env.PREFIX || '+' , //PREFIX, LEAVE IT DEFAULT OTHERWISE BUTTON WILL NOT WORK
 //////////////////////////////////////////////////////////
 
 WORK_TYPE: process.env.WORK_TYPE || 'public' , //public or private
@@ -104,7 +104,7 @@ MODERATORS : process.env.MODERATORS === undefined ? "923072380380" : process.env
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE, //Maximum size that bot can download a file   
 //////////////////////////////////////////////////////////
 
-OWNER_NAME: process.env.OWNER_NAME || 'Hamza' , //Your name
+OWNER_NAME: process.env.OWNER_NAME || 'HYDRA , //Your name
 //////////////////////////////////////////////////////////
 
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9' : process.env.POSTGRESQL_URL,
@@ -122,7 +122,7 @@ AUTO_REACT:  process.env.AUTO_REACT  || false  ,     //true or false
 AUTO_TYPING:  process.env.AUTO_TYPING  || false  , //true or false
 //////////////////////////////////////////////////////////
 
-AUTO_RECORDING:  process.env.AUTO_RECORDING  || false  ,  //true or false
+AUTO_RECORDING:  process.env.AUTO_RECORDING  || true  ,  //true or false
 //////////////////////////////////////////////////////////
 
 AUTO_READ:  process.env.AUTO_READ  || false  , //true or false
@@ -164,7 +164,7 @@ ANTI_CALL: process.env.ANTI_CALL || false  , //true or false
 ALIVE: process.env.ALIVE || `default`,     
 //////////////////////////////////////////////////////////
 
-AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,     //true or false
+AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || true  ,     //true or false
 //////////////////////////////////////////////////////////
 
 AUTO_STICKER: process.env.AUTO_STICKER || false  , //true or false
